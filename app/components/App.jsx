6ee820e439936b264709036f7869e47d7e2d60a6
Vasil Vanchuk <v.vanchuk@tut.by>
2015-10-17 20:47:35 +0300

    Add new items from onClick

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,18 +2,38 @@ import React, {Component} from 'react';
 import Notes from './Notes.jsx';
 import uuid from 'node-uuid';
 
-const notes = [
-  {id: uuid.v4(), task: 'Learn Webpack'},
-  {id: uuid.v4(), task: 'Learn React'},
-  {id: uuid.v4(), task: 'Do laundry'}
-];
-
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state =  {
+      notes: [
+        {id: uuid.v4(), task: 'Learn Webpack'},
+        {id: uuid.v4(), task: 'Learn React'},
+        {id: uuid.v4(), task: 'Do laundry'}
+      ]
+    };
+    
+    this.addNote = this.addNote.bind(this);
+  }
+
   render() {
+    const notes = this.state.notes;
+
     return (
       <div>
+        <button className="add-note" onClick={this.addNote}>+</button>
         <Notes items={notes} />
       </div>
     );
+  }
+
+  addNote() {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+      }])
+    });
   }
 }
