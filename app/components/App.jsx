cf71952bf3ea3f8cff4d41b8c9524d839c40de44
Vasil Vanchuk <v.vanchuk@tut.by>
2015-10-17 20:33:17 +0300

    Display list from react

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,26 @@
 import React, {Component} from 'react';
 import Note from './Note.jsx';
+import uuid from 'node-uuid';
+
+const notes = [
+  {id: uuid.v4(), task: 'Learn Webpack'},
+  {id: uuid.v4(), task: 'Learn React'},
+  {id: uuid.v4(), task: 'Do laundry'}
+];
 
 export default class App extends Component {
   render() {
-    return <Note />;
+    return (
+      <div>
+        <ul>{notes.map(this.renderNote)}</ul>
+      </div>
+    );
+  }
+  renderNote(note) {
+    return (
+      <li key={`note${note.id}`}>
+        <Note task={note.task} />
+      </li>
+    );
   }
 }
