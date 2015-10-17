948d733139b7ae645dc15601f1a5824b5ee43209
Vasil Vanchuk <v.vanchuk@tut.by>
2015-10-17 19:34:18 +0300

    Hello world

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+module.exports = {
+  entry: APP_PATH,
+  output: {
+    path: BUILD_PATH,
+    filename: 'bundle.js'
+  },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Kanban app'
+    })
+  ]
+};
